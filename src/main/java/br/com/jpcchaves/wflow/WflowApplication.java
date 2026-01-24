47b6da3ed0a070695bf9575cc2e1cbc366240399
jpcchaves <jpcchaves@outlook.com>
2026-01-24 11:58:04 -0300

    Initial commit

---
+++ src/main/java/br/com/jpcchaves/wflow/WflowApplication.java
@@ -0,0 +1,13 @@
+package br.com.jpcchaves.wflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jpcchaves/wflow/WflowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jpcchaves.wflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
